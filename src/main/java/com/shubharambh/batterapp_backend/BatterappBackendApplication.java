dea256eaa97bd094e3bef3ca8423ceaa9c70325b
Shrijay06 <shrijaynandgawkar06@gmail.com>
2025-07-13 16:22:55 +0530

    Initial commit

---
+++ src/main/java/com/shubharambh/batterapp_backend/BatterappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shubharambh.batterapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatterappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatterappBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/shubharambh/batterapp_backend/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.shubharambh.batterapp_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String sayHello() {
+        return "Hello from Batter App Backend!";
+    }
+}
